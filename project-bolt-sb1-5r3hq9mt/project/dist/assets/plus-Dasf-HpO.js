d2a1147f0d8fab4cd566fc8415fe9f742cb17cfa
freelance20071-glitch <freelance2007.1@gmail.com>
2026-07-24 02:48:51 +0300

    Updated package.json

---
+++ project-bolt-sb1-5r3hq9mt/project/dist/assets/plus-Dasf-HpO.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index-Bs2s3mYe.js";const s=a("Pencil",[["path",{d:"M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z",key:"5qss01"}],["path",{d:"m15 5 4 4",key:"1mk7zo"}]]);const t=a("Plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);export{s as P,t as a};
